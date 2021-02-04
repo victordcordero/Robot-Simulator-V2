3d7f2b997ec76b5ded2ec852c83bc904e6ec1b8b
github username <github email address>
2021-02-04 11:08:24 -0500

    DONEgit add .

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ document.addEventListener("DOMContentLoaded", function() {
     let moves = document.getElementById("moves-container");
     selectedMove = moves.removeChild(moves.childNodes[0])
     move((moves.childNodes[0]).innerText)
+    selectedMove.remove()
   }
   
 });
